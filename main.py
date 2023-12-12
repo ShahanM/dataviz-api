1dbd93f850a7820425f4d8a162494596501c972a
Shahan Iqbal <mehtabi@clemson.edu>
2023-12-12 01:41:17 -0500

    state data

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
 from fastapi import FastAPI
 import json
 from controllers.data_accesor import get_data_json
-from models.schema import NCEDItem, NCEDNerdItem
-from typing import List
-from data.datamap import state_id_file
+from data.datamap import state_id_file, state_id_datafile
 from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     root_path='/dataviz/api/',
 )
-
-# contact={
-#     'name': '',
-#     'url': '',
-#     'email': '',
-# },
-# license_info={
-#     'name': '',
-#     'url': '',
-# }
 
 
 origins = [
@@ -40,14 +29,23 @@ app.add_middleware(
 @app.get("/")
 async def root():
 	return {"message": "Hello World"}
+
+
 @app.get("/data/json/{year}")
 async def data_json(year: str):
-
 	return get_data_json(year)
+
+
+@app.get("/data/{stateid}")
+async def state_data(stateid: str):
+	if stateid in state_id_datafile:
+		with open('data/states/' + state_id_datafile[stateid]) as f:
+			d = json.load(f)
+	return {'data': d, 'key': state_id_datafile[stateid][:-5]}
+
+
 @app.get("/state/{stateid}")
 async def district_json(stateid: str):
-	# with open('data/tl_2019_45_unsd.json') as f:
-	# with open('data/testalabama.json') as f:
 	if stateid in state_id_file:
 		with open('data/district_topojson/' + state_id_file[stateid]) as f:
 			d = json.load(f)
